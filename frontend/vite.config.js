4314c312daa65bc61d7fbd2028c5333b047604d3
DariaSibirskaya <sdaryserg@mail.ru>
2024-10-22 14:07:02 +0300

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './frontend',  // Убедитесь, что путь корректен
-  build: {
-    rollupOptions: {
-      input: './frontend/index.html',  // Укажите путь к вашему index.html
-    },
-  },
-  optimizeDeps: {
-    include: [],  // Здесь можно указать зависимости, которые нужно предварительно собрать
-  }
 })
